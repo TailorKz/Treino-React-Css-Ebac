2b334c1ea922eacb8676773182bd689c866b26dc
MitsukiKz <tailorkunz@gmail.com>
2024-12-12 17:15:21 -0300

    progresso

--- src/Teste.tsx
+++ src/Teste.tsx
@@ -6,20 +6,29 @@ type BotaoProps = {
   fontSize: string
 }
 
-const Botao = styled.button`
-  background-color: ${(props) => (props.principal ? 'green' : 'blue')}
-  font-size: ${(props) => props.fontSize || '16px'}
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
 `
+
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+`
+
 function Teste() {
   return (
-    <div>
-      <Botao fontSize="18px" principal>
-        Clique Aqui
-      </Botao>
-      <Botao fontSize="14px" principal={false}>
-        Cancelar
-      </Botao>
-    </div>
+    <>
+      <div>
+        <Botao fontSize="18px" principal>
+          Clique Aqui
+        </Botao>
+        <Botao fontSize="14px" principal={false}>
+          Cancelar
+        </Botao>
+      </div>
+      <BotaoPerigo fontSize="16px">Não clique aqui</BotaoPerigo>
+    </>
   )
 }
 
